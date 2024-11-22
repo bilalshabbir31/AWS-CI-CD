5a06e7531d26f99c08dc3a01d353297b3e3f7450
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-11-22 12:52:05 +0400

    remove / endpoint

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const PORT = process.env.PORT
 const app = express()
 
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.status(200).send({ message: "Welcome to AWS CI-CD" });
-});
 app.use("/api", userRouter);
 app.use(notFound);
 app.listen(PORT, () => {
